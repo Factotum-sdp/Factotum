bdab3d475fb4c5b052a7d728c2baccc5f1d33ca2
Yanis <yanis.seddik@gmail.com>
2023-03-01 23:25:06 +0100

    use a new maps api key

--- app/src/main/java/com/github/factotum_sdp/factotum/MainActivity.kt
+++ app/src/main/java/com/github/factotum_sdp/factotum/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.github.factotum_sdp.factotum
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,8 +16,11 @@ class MainActivity : AppCompatActivity() {
         val button: Button = findViewById(R.id.validateButton)
 
         button.setOnClickListener {
-            val intent = Intent(this, MapsMarkerActivity::class.java)
-
+            val uri: String =
+                java.lang.String.format(Locale.ENGLISH,
+                    "google.navigation:q=EPFL,+Lausanne+Switzerland&mode=b")
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(uri))
+            intent.setPackage("com.google.android.apps.maps")
             startActivity(intent)
         }
     }

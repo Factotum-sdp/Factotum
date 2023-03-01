7c113ece8251d024fe6c9e9e13dc5dd5c00ae4c4
DanielTavaresA <comptesdaniel@gmail.com>
2023-03-01 11:51:08 +0100

    google sign in added

--- app/src/main/java/com/github/factotum_sdp/factotum/FirebaseUIActivity.kt
+++ app/src/main/java/com/github/factotum_sdp/factotum/FirebaseUIActivity.kt
@@ -3,14 +3,15 @@ package com.github.factotum_sdp.factotum
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.ErrorCodes
 import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
 import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
+import com.google.firebase.auth.FirebaseAuth
 
 abstract class FirebaseUIActivity : AppCompatActivity() {
     private val signInLauncher = registerForActivityResult(
         FirebaseAuthUIActivityResultContract()
-    ) { res ->
-        this.onSignInResult(res)
+    ) { res -> this.onSignInResult(res)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,20 +19,35 @@ abstract class FirebaseUIActivity : AppCompatActivity() {
         setContentView(R.layout.activity_firebase_uiactivity)
     }
 
-    // authentication providers
-    val providers = arrayListOf(
-        AuthUI.IdpConfig.GoogleBuilder().build(),
-        AuthUI.IdpConfig.EmailBuilder().build()
-    )
+    //create and launch the sign in intent
+    private fun createSignInIntent() {
+        // authentication providers
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.GoogleBuilder().build()
+        )
 
-    // create and lauch sign-in intent
-    val signInIntent = AuthUI.getInstance()
-        .createSignInIntentBuilder()
-        .setAvailableProviders(providers)
-        .build()
-    signInLaucher.lauch(signInIntent)
-    
+        // create and lauch sign-in intent
+        val signInIntent = AuthUI.getInstance()
+            .createSignInIntentBuilder()
+            .setAvailableProviders(providers)
+            .build()
+        signInLauncher.launch(signInIntent)
+    }
 
-
-
+    //callback on sign in
+    private fun onSignInResult(result : FirebaseAuthUIAuthenticationResult) {
+        val response = result.idpResponse
+        if (result.resultCode == RESULT_OK){
+            //successfully signed in
+            val user = FirebaseAuth.getInstance().currentUser
+        } else {
+            // sign in failed
+            if (response == null) {
+                // authentication canceled
+            } else {
+                val error = response.error
+                print(error!!.message)
+            }
+        }
+    }
 }

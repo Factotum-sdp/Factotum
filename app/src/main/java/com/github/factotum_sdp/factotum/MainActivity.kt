434d8da2bf98fad6cb5d3401c829c5b3796b316e
Yanis <yanis.seddik@gmail.com>
2023-03-01 20:34:50 +0100

    refactoring

--- app/src/main/java/com/github/factotum_sdp/factotum/MainActivity.kt
+++ app/src/main/java/com/github/factotum_sdp/factotum/MainActivity.kt
@@ -3,7 +3,6 @@ package com.github.factotum_sdp.factotum
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
-import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
--- app/src/main/java/com/github/factotum_sdp/factotum/MapsMarkerActivity.kt
+++ app/src/main/java/com/github/factotum_sdp/factotum/MapsMarkerActivity.kt
@@ -1,7 +1,6 @@
 package com.github.factotum_sdp.factotum
 
 import android.os.Bundle
-import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.maps.CameraUpdateFactory

8d7775cc6ebc1906af55c0a3311e440442173082
DanielTavaresA <comptesdaniel@gmail.com>
2023-03-02 10:11:04 +0100

    Logged in activity added

--- app/src/main/java/com/github/factotum_sdp/factotum/FirebaseUIActivity.kt
+++ app/src/main/java/com/github/factotum_sdp/factotum/FirebaseUIActivity.kt
@@ -1,14 +1,18 @@
 package com.github.factotum_sdp.factotum
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.ErrorCodes
 import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
 import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
 import com.google.firebase.auth.FirebaseAuth
 
-abstract class FirebaseUIActivity : AppCompatActivity() {
+class FirebaseUIActivity : AppCompatActivity() {
     private val signInLauncher = registerForActivityResult(
         FirebaseAuthUIActivityResultContract()
     ) { res -> this.onSignInResult(res)
@@ -17,6 +21,10 @@ abstract class FirebaseUIActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_firebase_uiactivity)
+        val login: Button = findViewById(R.id.Login)
+        login.setOnClickListener {
+            createSignInIntent()
+        }
     }
 
     //create and launch the sign in intent
@@ -37,16 +45,21 @@ abstract class FirebaseUIActivity : AppCompatActivity() {
     //callback on sign in
     private fun onSignInResult(result : FirebaseAuthUIAuthenticationResult) {
         val response = result.idpResponse
+
         if (result.resultCode == RESULT_OK){
             //successfully signed in
+
             val user = FirebaseAuth.getInstance().currentUser
+            val intent = Intent(this, LoggedIn::class.java)
+            intent.putExtra("userEmail", user.email)
+            intent.putExtra("userName", user.displayName)
+            startActivity(intent)
         } else {
             // sign in failed
             if (response == null) {
                 // authentication canceled
             } else {
                 val error = response.error
-                print(error!!.message)
             }
         }
     }

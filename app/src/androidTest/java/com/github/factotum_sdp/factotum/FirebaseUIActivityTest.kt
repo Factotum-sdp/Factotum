3d10c900a646c4284d6c705112f5cd85249faf3d
DanielTavaresA <comptesdaniel@gmail.com>
2023-03-02 11:49:25 +0100

    Interface added

--- app/src/androidTest/java/com/github/factotum_sdp/factotum/FirebaseUIActivityTest.kt
+++ app/src/androidTest/java/com/github/factotum_sdp/factotum/FirebaseUIActivityTest.kt
@@ -1,4 +1,30 @@
 package com.github.factotum_sdp.factotum
 
+import android.content.Intent
+import androidx.test.core.app.ActivityScenario
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.intent.matcher.IntentMatchers
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import org.hamcrest.core.AllOf
+import org.junit.Rule
+import org.junit.Test
+
 class FirebaseUIActivityTest {
+    @get:Rule
+    var testRule = ActivityScenarioRule(
+        FirebaseUIActivity::class.java
+    )
+
+    @Test
+    fun correctLogIn(){
+        val firebaseUI :FirebaseUIActivity  = FirebaseUIActivity()
+        firebaseUI.createSignInIntent()
+
+
+    }
 }
--- app/src/main/java/com/github/factotum_sdp/factotum/FirebaseUI.kt
+++ app/src/main/java/com/github/factotum_sdp/factotum/FirebaseUI.kt
@@ -1,4 +1,8 @@
 package com.github.factotum_sdp.factotum
 
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
+
 interface FirebaseUI {
+    fun onSignInResult(result : FirebaseAuthUIAuthenticationResult)
+    fun createSignInIntent()
 }

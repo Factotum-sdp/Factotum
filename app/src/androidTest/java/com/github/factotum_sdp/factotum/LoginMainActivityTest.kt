1018224a7140a0f6a69895ac6802e8d130ecc2f0
Yanis <yanis.seddik@gmail.com>
2023-03-07 19:12:29 +0100

    clean unnecessary code

---
+++ app/src/androidTest/java/com/github/factotum_sdp/factotum/LoginMainActivityTest.kt
@@ -0,0 +1,31 @@
+package com.github.factotum_sdp.factotum
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.intent.Intents.intended
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasExtra
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.hamcrest.core.AllOf.allOf
+
+
+@RunWith(AndroidJUnit4::class)
+class LoginMainActivityTest {
+
+    private val userName = "Carl"
+
+    @get:Rule
+    var testRule = ActivityScenarioRule(
+        LoginMainActivity::class.java
+    )
+}
---
+++ app/src/main/java/com/github/factotum_sdp/factotum/LoginMainActivity.kt
@@ -0,0 +1,15 @@
+package com.github.factotum_sdp.factotum
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginMainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

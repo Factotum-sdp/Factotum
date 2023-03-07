41b66af1ad3d2c6b050045be7f76f2eb14c4f5b7
Yanis <yanis.seddik@gmail.com>
2023-03-07 19:25:26 +0100

    change implementation versions and add google auth implementation

---
+++ app/src/main/java/com/github/factotum_sdp/factotum/login/data/model/LoggedInUser.kt
@@ -0,0 +1,9 @@
+package com.github.factotum_sdp.factotum.login.data.model
+
+/**
+ * Data class that captures user information for logged in users retrieved from LoginRepository
+ */
+data class LoggedInUser(
+    val userId: String,
+    val displayName: String
+)

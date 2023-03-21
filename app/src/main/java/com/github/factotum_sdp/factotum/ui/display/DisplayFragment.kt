9a1f6447076de14ba7f0287f9c95aca650916529
Jules PERRIN <jules.perrin@epfl.ch>
2023-03-21 14:18:58 +0100

    Now using firebaseui storage for displaying the images, should result in faster loading times after caching the data and better async loading overall. Still have to make test and make a better ui

--- app/src/main/java/com/github/factotum_sdp/factotum/ui/display/DisplayFragment.kt
+++ app/src/main/java/com/github/factotum_sdp/factotum/ui/display/DisplayFragment.kt
@@ -10,12 +10,14 @@ import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
 import com.github.factotum_sdp.factotum.databinding.DisplayItemBinding
 import com.github.factotum_sdp.factotum.databinding.FragmentDisplayBinding
+import com.google.firebase.storage.StorageReference
 
+// Fragment responsible for displaying a list of images from Firebase Storage
 class DisplayFragment : Fragment() {
 
+    // View model for this fragment
     private lateinit var viewModel: DisplayViewModel
     private var _binding: FragmentDisplayBinding? = null
     private val binding get() = _binding!!
@@ -24,18 +26,22 @@ class DisplayFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        // Inflate the fragment layout
         _binding = FragmentDisplayBinding.inflate(inflater, container, false)
 
+        // Initialize the view model
         viewModel = ViewModelProvider(this)[DisplayViewModel::class.java]
 
+        // Set up the recycler view with a photo adapter
         val photoAdapter = PhotoAdapter()
         binding.recyclerView.apply {
             layoutManager = LinearLayoutManager(context)
             adapter = photoAdapter
         }
 
-        viewModel.photoUrls.observe(viewLifecycleOwner) { photoUrls ->
-            photoAdapter.submitList(photoUrls)
+        // Observe changes in the list of photo references and update the adapter
+        viewModel.photoReferences.observe(viewLifecycleOwner) { photoReferences ->
+            photoAdapter.submitList(photoReferences)
         }
 
         // Set up the refresh button click listener
@@ -46,12 +52,14 @@ class DisplayFragment : Fragment() {
         return binding.root
     }
 
+    // Clean up binding when the view is destroyed
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
 
-    inner class PhotoAdapter : ListAdapter<String, PhotoViewHolder>(PhotoDiffCallback()) {
+    // Adapter for displaying photos in the recycler view
+    inner class PhotoAdapter : ListAdapter<StorageReference, PhotoViewHolder>(PhotoDiffCallback()) {
 
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PhotoViewHolder {
             val binding = DisplayItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
@@ -63,20 +71,23 @@ class DisplayFragment : Fragment() {
         }
     }
 
-    inner class PhotoDiffCallback : DiffUtil.ItemCallback<String>() {
-        override fun areItemsTheSame(oldItem: String, newItem: String): Boolean {
-            return oldItem == newItem
+    // Callback to calculate the difference between two photo items
+    inner class PhotoDiffCallback : DiffUtil.ItemCallback<StorageReference>() {
+        override fun areItemsTheSame(oldItem: StorageReference, newItem: StorageReference): Boolean {
+            return oldItem.path == newItem.path
         }
 
-        override fun areContentsTheSame(oldItem: String, newItem: String): Boolean {
-            return oldItem == newItem
+        override fun areContentsTheSame(oldItem: StorageReference, newItem: StorageReference): Boolean {
+            return oldItem.path == newItem.path
         }
     }
 
+    // ViewHolder for displaying an individual photo item
     inner class PhotoViewHolder(private val binding: DisplayItemBinding) : RecyclerView.ViewHolder(binding.root) {
-        fun bind(url: String) {
-            Glide.with(binding.displayItemView.context)
-                .load(url)
+        fun bind(storageReference: StorageReference) {
+            // Load the image using Glide and display it in the ImageView
+            GlideApp.with(binding.displayItemView.context)
+                .load(storageReference)
                 .into(binding.displayItemView)
         }
     }
--- app/src/main/java/com/github/factotum_sdp/factotum/ui/display/DisplayViewModel.kt
+++ app/src/main/java/com/github/factotum_sdp/factotum/ui/display/DisplayViewModel.kt
@@ -1,67 +1,72 @@
 package com.github.factotum_sdp.factotum.ui.display
+
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
+// ViewModel for managing the display of images from Firebase Storage
 class DisplayViewModel : ViewModel() {
-    private val _photoUrls = MutableLiveData<List<String>>()
-    val photoUrls: LiveData<List<String>>
-        get() = _photoUrls
+    // LiveData to store the list of storage references for the images
+    private val _photoReferences = MutableLiveData<List<StorageReference>>()
+    val photoReferences: LiveData<List<StorageReference>>
+        get() = _photoReferences
 
+    // Reference to the Firebase Storage instance
     private val storage = FirebaseStorage.getInstance()
 
-    init { fetchPhotoUrls() }
+    init {
+        // Fetch the photo references when the ViewModel is initialized
+        fetchPhotoReferences()
+    }
 
-    private fun fetchPhotoUrls() {
+    // Fetch photo references from Firebase Storage
+    private fun fetchPhotoReferences() {
         CoroutineScope(Dispatchers.IO).launch {
-            val photoList = mutableListOf<String>()
+            val photoList = mutableListOf<StorageReference>()
             val storageRef = storage.reference
 
+            // List all items in the storage and process the results
             storageRef.listAll().addOnSuccessListener { listResult ->
                 val photoRefs = listResult.items.sortedBy { it.name }
-
                 photoRefs.forEach { photoRef ->
-                    photoRef.downloadUrl.addOnSuccessListener { uri ->
-                        photoList.add(uri.toString())
+                    photoList.add(photoRef)
 
-                        if (photoList.size == photoRefs.size) {
-                            // All items have been processed, update the LiveData
-                            _photoUrls.postValue(photoList)
-                        }
+                    if (photoList.size == photoRefs.size) {
+                        // All items have been processed, update the LiveData
+                        _photoReferences.postValue(photoList)
                     }
                 }
             }
         }
     }
 
-
+    // Refresh the list of images from Firebase Storage
     fun refreshImages() {
         CoroutineScope(Dispatchers.IO).launch {
-            val newPhotoList = mutableListOf<String>()
+            val newPhotoList = mutableListOf<StorageReference>()
             val storageRef = storage.reference
 
+            // List all items in the storage and process the results
             storageRef.listAll().addOnSuccessListener { listResult ->
                 val photoRefs = listResult.items.sortedBy { it.name }
                 val itemsCount = photoRefs.size
                 var itemsProcessed = 0
 
                 photoRefs.forEach { photoRef ->
-                    photoRef.downloadUrl.addOnSuccessListener { uri ->
-                        newPhotoList.add(uri.toString())
-                        itemsProcessed++
+                    newPhotoList.add(photoRef)
+                    itemsProcessed++
 
-                        if (itemsProcessed == itemsCount) {
-                            // All items have been processed, update the LiveData
-                            _photoUrls.postValue(newPhotoList)
-                        }
+                    if (itemsProcessed == itemsCount) {
+                        // All items have been processed, update the LiveData
+                        _photoReferences.postValue(newPhotoList)
                     }
                 }
             }
         }
     }
-
 }

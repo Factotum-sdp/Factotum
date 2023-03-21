9bb7605ab51f65f999858bdcebe7136c2a440b96
Yanis <yanis.seddik@gmail.com>
2023-03-21 20:04:32 +0100

    add previous login code

--- app/src/main/java/com/github/factotum_sdp/factotum/data/LoginDataSource.kt
+++ app/src/main/java/com/github/factotum_sdp/factotum/data/LoginDataSource.kt
@@ -10,11 +10,16 @@ class LoginDataSource {
 
     fun login(username: String, password: String): Result<LoggedInUser> {
         try {
+            // TODO: handle loggedInUser authentication
             val fakeUser = LoggedInUser(java.util.UUID.randomUUID().toString(), username)
             return Result.Success(fakeUser)
         } catch (e: Throwable) {
             return Result.Error(IOException("Error logging in", e))
         }
+    }
+
+    fun logout() {
+        // TODO: revoke authentication
     }
 
 }

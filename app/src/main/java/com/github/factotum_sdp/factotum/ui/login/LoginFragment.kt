e247e5b2096058e0271e21fa3a65b976a3ed4f64
Yanis <yanis.seddik@gmail.com>
2023-03-21 19:55:23 +0100

    add necessary for login files that were deleted

--- app/src/main/java/com/github/factotum_sdp/factotum/ui/login/LoginFragment.kt
+++ app/src/main/java/com/github/factotum_sdp/factotum/ui/login/LoginFragment.kt
@@ -6,10 +6,8 @@ import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.inputmethod.EditorInfo
 import android.widget.Button
 import android.widget.EditText
-import android.widget.ProgressBar
 import androidx.annotation.StringRes
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -101,6 +99,16 @@ class LoginFragment : Fragment() {
     ) {
         usernameEditText.addTextChangedListener(afterTextChangedListener)
         passwordEditText.addTextChangedListener(afterTextChangedListener)
+    }
+
+    private fun updateUiWithUser(model: LoggedInUserView) {
+        val welcome = getString(R.string.welcome) + " " + model.displayName
+        // TODO : initiate successful logged in experience
+        Snackbar.make(requireView(), welcome, Snackbar.LENGTH_LONG).show()
+    }
+
+    private fun showLoginFailed(@StringRes errorString: Int) {
+        Snackbar.make(requireView(), errorString, Snackbar.LENGTH_LONG).show()
     }
 
     override fun onDestroyView() {

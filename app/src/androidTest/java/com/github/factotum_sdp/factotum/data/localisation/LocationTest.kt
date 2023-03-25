d3d33914b2fca214c343b82c54c088803dd4abf6
DanielTavaresA <comptesdaniel@gmail.com>
2023-03-25 16:20:47 +0100

    handle SDK version in tests

--- app/src/androidTest/java/com/github/factotum_sdp/factotum/data/localisation/LocationTest.kt
+++ app/src/androidTest/java/com/github/factotum_sdp/factotum/data/localisation/LocationTest.kt
@@ -1,6 +1,8 @@
 package com.github.factotum_sdp.factotum.data.localisation
 
+import android.location.Address
 import android.location.Geocoder
+import android.os.Build
 import androidx.test.core.app.ApplicationProvider.getApplicationContext
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import junit.framework.TestCase.assertEquals
@@ -15,14 +17,20 @@ class LocationTest {
         val addressName = "Route Cantonale 15, 1015 Lausanne"
         val location = Location(addressName, getApplicationContext())
         val geocoder =  Geocoder(getApplicationContext())
+        var result : Address? = null
         val latch = CountDownLatch(1)
-        geocoder.getFromLocationName(addressName, 1) { addresses ->
-            val result = if(addresses.size != 0) addresses[0] else null
-            assertEquals(location.address!!.latitude, result!!.latitude)
-            assertEquals(location.address!!.longitude, result.longitude)
-            latch.countDown()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            geocoder.getFromLocationName(addressName, 1) { addresses ->
+                result = if(addresses.size != 0) addresses[0] else null
+                latch.countDown()
+            }
+            latch.await()
+        } else {
+            result = geocoder.getFromLocationName(addressName, 1)?.get(0)
         }
-        latch.await()
+        assertEquals(location.address!!.latitude, result!!.latitude)
+        assertEquals(location.address!!.longitude, result!!.longitude)
+
     }
 
     @Test

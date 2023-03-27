96eadfa21b084d8fb099072d6769b85f6b49cd8c
Jules PERRIN <jules.perrin@epfl.ch>
2023-03-27 20:48:31 +0200

    I've never seen something that horrible, but the feature works well so, but difficult to test whatsapp, or sms on the CI...

--- app/src/main/java/com/github/factotum_sdp/factotum/ui/display/DisplayFragment.kt
+++ app/src/main/java/com/github/factotum_sdp/factotum/ui/display/DisplayFragment.kt
@@ -1,15 +1,15 @@
 package com.github.factotum_sdp.factotum.ui.display
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.github.factotum_sdp.factotum.databinding.DisplayItemBinding
+import com.github.factotum_sdp.factotum.R
 import com.github.factotum_sdp.factotum.databinding.FragmentDisplayBinding
 import com.github.factotum_sdp.factotum.ui.display.utils.PhotoAdapter
 import com.google.firebase.storage.StorageReference
@@ -30,7 +30,9 @@ class DisplayFragment : Fragment() {
         _binding = FragmentDisplayBinding.inflate(inflater, container, false)
 
         // Set up the recycler view with a photo adapter
-        val photoAdapter = PhotoAdapter()
+        val photoAdapter = PhotoAdapter() { storageReference ->
+            shareImage(storageReference, "1234567890")
+        }
         binding.recyclerView.apply {
             layoutManager = LinearLayoutManager(context)
             adapter = photoAdapter
@@ -54,4 +56,39 @@ class DisplayFragment : Fragment() {
         super.onDestroyView()
         _binding = null
     }
+
+    private fun shareImage(storageReference: StorageReference, phoneNumber: String) {
+        storageReference.downloadUrl.addOnSuccessListener { uri ->
+            val shareText = "Here is your delivery: $uri"
+
+            // General sharing intent
+            val generalShareIntent = Intent(Intent.ACTION_SEND).apply {
+                type = "text/plain"
+                putExtra(Intent.EXTRA_TEXT, shareText)
+            }
+
+            // Intent for SMS sharing
+            val smsIntent = Intent(Intent.ACTION_SENDTO).apply {
+                data = Uri.parse("smsto:$phoneNumber")
+                putExtra("sms_body", shareText)
+            }
+
+            // Intent for WhatsApp sharing
+            val whatsappIntent = Intent(Intent.ACTION_VIEW).apply {
+                data = Uri.parse("https://api.whatsapp.com/send?phone=$phoneNumber&text=$shareText")
+                setPackage("com.whatsapp")
+            }
+
+            // Create a chooser intent with the option to share via general sharing options and WhatsApp
+            val chooserIntent = Intent.createChooser(generalShareIntent, getString(R.string.share)).apply {
+                putExtra(Intent.EXTRA_INITIAL_INTENTS, arrayOf(whatsappIntent, smsIntent))
+            }
+
+
+            startActivity(chooserIntent)
+        }.addOnFailureListener {
+            // Handle failure here
+        }
+    }
+
 }
--- app/src/main/java/com/github/factotum_sdp/factotum/ui/display/utils/PhotoDiffCallback.kt
+++ app/src/main/java/com/github/factotum_sdp/factotum/ui/display/utils/PhotoDiffCallback.kt
@@ -5,6 +5,7 @@ import com.google.firebase.storage.StorageReference
 
 // Callback to calculate the difference between two photo items
 class PhotoDiffCallback : DiffUtil.ItemCallback<StorageReference>() {
+
     override fun areItemsTheSame(oldItem: StorageReference, newItem: StorageReference): Boolean {
         return oldItem.path == newItem.path
     }

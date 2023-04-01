305b11373d885b5d87cc59dee3884402b1bf445d
Yanis <yanis.seddik@gmail.com>
2023-04-01 20:56:48 +0200

    for ci no actual change

--- app/src/main/java/com/github/factotum_sdp/factotum/data/LoginRepository.kt
+++ app/src/main/java/com/github/factotum_sdp/factotum/data/LoginRepository.kt
@@ -4,15 +4,10 @@ package com.github.factotum_sdp.factotum.data
  * Class that requests authentication and user information from the remote data source and
  * maintains an in-memory cache of login status and user credentials information.
  */
-
 class LoginRepository(val dataSource: LoginDataSource) {
 
     // in-memory cache of the loggedInUser object
-    var user: LoggedInUser? = null
-        private set
-
-    val isLoggedIn: Boolean
-        get() = user != null
+    private var user: LoggedInUser? = null
 
     init {
         // If user credentials will be cached in local storage, it is recommended it be encrypted

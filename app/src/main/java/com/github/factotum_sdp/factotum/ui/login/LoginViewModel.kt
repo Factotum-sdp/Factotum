e8cc9fdec9eab3d01665f8f34e52a1cff4d110d7
Yanis <yanis.seddik@gmail.com>
2023-04-02 13:23:13 +0200

    restore login function & loginResult variable

--- app/src/main/java/com/github/factotum_sdp/factotum/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/github/factotum_sdp/factotum/ui/login/LoginViewModel.kt
@@ -3,14 +3,43 @@ package com.github.factotum_sdp.factotum.ui.login
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.github.factotum_sdp.factotum.R
 import com.github.factotum_sdp.factotum.data.LoginRepository
 import com.github.factotum_sdp.factotum.ui.auth.BaseAuthState
+import com.github.factotum_sdp.factotum.data.Result
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class LoginViewModel(private val loginRepository: LoginRepository) : ViewModel() {
 
     private val _loginForm = MutableLiveData<LoginFormState>()
     val loginFormState: LiveData<LoginFormState> = _loginForm
+
+    private val _loginResult = MutableLiveData<LoginResult>()
+    val loginResult: LiveData<LoginResult> = _loginResult
+
+    private val dispatcher: CoroutineDispatcher = Dispatchers.IO
+
+    fun login(userEmail: String, password: String) {
+        // launch in a separate asynchronous job
+        viewModelScope.launch {
+            val result = withContext(dispatcher) { loginRepository.login(userEmail, password) }
+            if (result is Result.Success) {
+                _loginResult.value =
+                    LoginResult(
+                        success = LoggedInUserView(
+                            displayName = result.data.displayName,
+                            email = result.data.email
+                        )
+                    )
+            } else {
+                _loginResult.value = LoginResult(error = R.string.login_failed)
+            }
+        }
+    }
 
     fun loginDataChanged(email: String, password: String) {
         if (!BaseAuthState.isEmailValid(email)) {

d6c5ffd36b06457e26023731509321dcc035f34c
Yanis <yanis.seddik@gmail.com>
2023-04-04 01:11:09 +0200

    adapt tests to ci

--- app/src/androidTest/java/com/github/factotum_sdp/factotum/ui/signup/SignUpFragmentTest.kt
+++ app/src/androidTest/java/com/github/factotum_sdp/factotum/ui/signup/SignUpFragmentTest.kt
@@ -2,16 +2,14 @@ package com.github.factotum_sdp.factotum.ui.signup
 
 import androidx.test.espresso.Espresso.onData
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions
-import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.action.ViewActions.closeSoftKeyboard
-import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.RootMatchers
-import androidx.test.espresso.matcher.ViewMatchers
-import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.RootMatchers.isPlatformPopup
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.platform.app.InstrumentationRegistry.getInstrumentation
 import androidx.test.uiautomator.UiDevice
 import com.github.factotum_sdp.factotum.MainActivity
 import com.github.factotum_sdp.factotum.R
@@ -37,37 +35,39 @@ class SignUpFragmentTest {
 
     @Test
     fun signUpFormInitialStateIsEmpty() {
-        onView(withId(R.id.username)).check(ViewAssertions.matches(ViewMatchers.withText("")))
-        onView(withId(R.id.email)).check(ViewAssertions.matches(ViewMatchers.withText("")))
-        onView(withId(R.id.password)).check(ViewAssertions.matches(ViewMatchers.withText("")))
-        onView(withId(R.id.role)).check(ViewAssertions.matches(ViewMatchers.withText("")))
-        onView(withId(R.id.signup)).check(ViewAssertions.matches(not(ViewMatchers.isEnabled())))
+        onView(withId(R.id.username)).check(matches(withText("")))
+        onView(withId(R.id.email)).check(matches(withText("")))
+        onView(withId(R.id.password)).check(matches(withText("")))
+        onView(withId(R.id.role)).check(matches(withText("")))
+        onView(withId(R.id.signup)).check(matches(not(isEnabled())))
     }
 
     @Test
     fun signUpFormWithoutRole() {
-        onView(withId(R.id.username)).perform(ViewActions.typeText("Jane Doe"))
+        onView(withId(R.id.username)).perform(typeText("Jane Doe"))
         onView(withId(R.id.fragment_signup_directors_parent)).perform(
             closeSoftKeyboard()
         )
-        onView(withId(R.id.email)).perform(ViewActions.typeText("jane.doe@gmail.com"))
+        onView(withId(R.id.email)).perform(typeText("jane.doe@gmail.com"))
         onView(withId(R.id.fragment_signup_directors_parent)).perform(
             closeSoftKeyboard()
         )
-        onView(withId(R.id.password)).perform(ViewActions.typeText("123456"))
+        onView(withId(R.id.password)).perform(typeText("123456"))
         onView(withId(R.id.fragment_signup_directors_parent)).perform(
             closeSoftKeyboard()
         )
-        onView(withId(R.id.signup)).check(ViewAssertions.matches(not(ViewMatchers.isEnabled())))
+        onView(withId(R.id.role)).perform(click())
+        onView(withId(R.id.role)).perform(click())
+        onView(withId(R.id.signup)).check(matches(not(isEnabled())))
     }
 
     @Test
     fun signUpFormWithoutPassword() {
-        onView(withId(R.id.username)).perform(ViewActions.typeText("Jane Doe"))
+        onView(withId(R.id.username)).perform(typeText("Jane Doe"))
         onView(withId(R.id.fragment_signup_directors_parent)).perform(
             closeSoftKeyboard()
         )
-        onView(withId(R.id.email)).perform(ViewActions.typeText("jane.doe@gmail.com"))
+        onView(withId(R.id.email)).perform(typeText("jane.doe@gmail.com"))
         onView(withId(R.id.fragment_signup_directors_parent)).perform(
             closeSoftKeyboard()
         )
@@ -75,17 +75,17 @@ class SignUpFragmentTest {
             closeSoftKeyboard()
         )
         onView(withId(R.id.role)).perform(click())
-        onData(anything()).inRoot(RootMatchers.isPlatformPopup()).atPosition(1).perform(click())
-        onView(withId(R.id.signup)).check(ViewAssertions.matches(not(ViewMatchers.isEnabled())))
+        onData(anything()).inRoot(isPlatformPopup()).atPosition(1).perform(click())
+        onView(withId(R.id.signup)).check(matches(not(isEnabled())))
     }
 
     @Test
     fun signUpFormWithoutEmail() {
-        onView(withId(R.id.username)).perform(ViewActions.typeText("Jane Doe"))
+        onView(withId(R.id.username)).perform(typeText("Jane Doe"))
         onView(withId(R.id.fragment_signup_directors_parent)).perform(
             closeSoftKeyboard()
         )
-        onView(withId(R.id.password)).perform(ViewActions.typeText("123456"))
+        onView(withId(R.id.password)).perform(typeText("123456"))
         onView(withId(R.id.fragment_signup_directors_parent)).perform(
             closeSoftKeyboard()
         )
@@ -93,48 +93,52 @@ class SignUpFragmentTest {
             closeSoftKeyboard()
         )
         onView(withId(R.id.role)).perform(click())
-        onData(anything()).inRoot(RootMatchers.isPlatformPopup()).atPosition(1).perform(click())
-        onView(withId(R.id.signup)).check(ViewAssertions.matches(not(ViewMatchers.isEnabled())))
+        onData(anything()).inRoot(isPlatformPopup()).atPosition(1).perform(click())
+        onView(withId(R.id.signup)).check(matches(not(isEnabled())))
     }
 
     @Test
     fun signUpFormWithoutUsername() {
-        onView(withId(R.id.email)).perform(ViewActions.typeText("jane.doe@gmail.com"))
+        onView(withId(R.id.email)).perform(typeText("jane.doe@gmail.com"))
         onView(withId(R.id.fragment_signup_directors_parent)).perform(
             closeSoftKeyboard()
         )
-        onView(withId(R.id.password)).perform(ViewActions.typeText("123456"))
+        onView(withId(R.id.password)).perform(typeText("123456"))
         onView(withId(R.id.fragment_signup_directors_parent)).perform(
             closeSoftKeyboard()
         )
         onView(withId(R.id.role)).perform(click())
         onData(anything()).inRoot(RootMatchers.isPlatformPopup()).atPosition(1).perform(click())
-        onView(withId(R.id.signup)).check(ViewAssertions.matches(not(ViewMatchers.isEnabled())))
+        onView(withId(R.id.signup)).check(matches(not(isEnabled())))
     }
 
     @Test
     fun signUpFormWithAllFields() {
-        onView(withId(R.id.username)).perform(ViewActions.typeText("Jane Doe"))
+        onView(withId(R.id.username)).perform(typeText("Jane Doe"))
         onView(withId(R.id.fragment_signup_directors_parent)).perform(
             closeSoftKeyboard()
         )
-        onView(withId(R.id.email)).perform(ViewActions.typeText("jane.doe@gmail.com"))
+        onView(withId(R.id.email)).perform(typeText("jane.doe@gmail.com"))
         onView(withId(R.id.fragment_signup_directors_parent)).perform(
             closeSoftKeyboard()
         )
-        onView(withId(R.id.password)).perform(ViewActions.typeText("123456"))
+        onView(withId(R.id.password)).perform(typeText("123456"))
         onView(withId(R.id.fragment_signup_directors_parent)).perform(
             closeSoftKeyboard()
         )
         onView(withId(R.id.role)).perform(click())
-        onData(anything()).inRoot(RootMatchers.isPlatformPopup()).atPosition(1).perform(click())
-        onView(withId(R.id.signup)).check(ViewAssertions.matches(ViewMatchers.isEnabled()))
+        onData(anything()).inRoot(isPlatformPopup()).atPosition(1).perform(click())
+        onView(withId(R.id.signup)).check(matches(isEnabled()))
     }
 
     @Test
-    fun clickOnSignUpLeadsToLoginFragment() {
-        val device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
+    fun clickOnBackLeadsToLoginFragment() {
+        val device = UiDevice.getInstance(getInstrumentation())
         device.pressBack()
-        onView(withId(R.id.fragment_login_directors_parent)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        onView(withId(R.id.fragment_login_directors_parent)).check(
+            matches(
+                isDisplayed()
+            )
+        )
     }
 }

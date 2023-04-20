f3484af2d5b09dcc6737c8f7063a825d1bd714ec
DanielTavaresA <comptesdaniel@gmail.com>
2023-04-20 13:32:51 +0200

    add permissions in tests

--- app/src/androidTest/java/com/github/factotum_sdp/factotum/ui/directory/ContactDetailsFragmentTest.kt
+++ app/src/androidTest/java/com/github/factotum_sdp/factotum/ui/directory/ContactDetailsFragmentTest.kt
@@ -1,5 +1,6 @@
 package com.github.factotum_sdp.factotum.ui.directory
 
+import android.Manifest
 import android.content.Intent
 import androidx.recyclerview.widget.RecyclerView
 import androidx.test.espresso.Espresso.onView
@@ -13,6 +14,7 @@ import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.rule.GrantPermissionRule
 import androidx.test.uiautomator.By
 import androidx.test.uiautomator.UiDevice
 import androidx.test.uiautomator.Until.hasObject
@@ -36,6 +38,11 @@ import org.junit.runner.RunWith
 class ContactDetailsFragmentTest {
     @get:Rule
     var activityRule = ActivityScenarioRule(MainActivity::class.java)
+
+    @get:Rule
+    val permission = GrantPermissionRule.grant(
+        Manifest.permission.ACCESS_FINE_LOCATION,
+        Manifest.permission.ACCESS_COARSE_LOCATION)
 
     companion object {
         @BeforeClass
@@ -124,7 +131,7 @@ class ContactDetailsFragmentTest {
     fun buttonShowDestination(){
         onView(withId(R.id.show_all_button)).perform(click())
         val device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
-        val markers = device.wait(hasObject(By.descContains("Destination")), 5000)
+        val markers = device.wait(hasObject(By.descContains("Destination")), 5000L)
         assertTrue(markers)
     }
 }

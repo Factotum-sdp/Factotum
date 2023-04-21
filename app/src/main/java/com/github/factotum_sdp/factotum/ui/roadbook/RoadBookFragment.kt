fd087453786390132e5dd6fbf0f85b0697a7bfe0
TimVW7 <49046817+TimVW7@users.noreply.github.com>
2023-04-22 00:58:38 +0200

    Fixed movingOutsideRBFragmentKeepsButtonStates by saving Button states when on Pause

--- app/src/main/java/com/github/factotum_sdp/factotum/ui/roadbook/RoadBookFragment.kt
+++ app/src/main/java/com/github/factotum_sdp/factotum/ui/roadbook/RoadBookFragment.kt
@@ -63,6 +63,11 @@ class RoadBookFragment : Fragment(), MenuProvider {
 
     override fun onPause() {
         rbViewModel.backUp()
+        saveRadioButtonState(SWIPE_R_SHARED_KEY, R.id.rbSwipeREdition)
+        saveRadioButtonState(SWIPE_L_SHARED_KEY, R.id.rbSwipeLDeletion)
+        saveRadioButtonState(DRAG_N_DROP_SHARED_KEY, R.id.rbDragDrop)
+        saveRadioButtonState(TOUCH_CLICK_SHARED_KEY, R.id.rbTouchClick)
+        saveRadioButtonState(SHOW_ARCHIVED_KEY, R.id.showArchived)
         super.onPause()
     }
 

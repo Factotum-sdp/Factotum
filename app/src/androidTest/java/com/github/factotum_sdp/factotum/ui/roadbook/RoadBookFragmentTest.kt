7d127e0139a372c21d03961abdd52ab589e8ac0f
Carl <carl.schutz@epfl.ch>
2023-04-22 19:21:57 +0200

    One test for foreground location service

--- app/src/androidTest/java/com/github/factotum_sdp/factotum/ui/roadbook/RoadBookFragmentTest.kt
+++ app/src/androidTest/java/com/github/factotum_sdp/factotum/ui/roadbook/RoadBookFragmentTest.kt
@@ -709,29 +709,33 @@ class RoadBookFragmentTest {
     }
 
     @Test
-    fun automaticTimestampIsWorkingWhenFragmentIsOnTheBackGround() {
+    fun automaticTimestampIsWorkingWhenNavigatingInTheApp() {
         // Non timestamped record, hence swipe left shows deletion dialog
-        swipeLeftTheRecordAt(2)
+        swipeLeftTheRecordAt(1)
         onView(withText(R.string.delete_dialog_title)).check(matches(isDisplayed()))
         Thread.sleep(3000)
         onView(withText(R.string.swipeleft_cancel_button_label)).perform(click())
-        onView(withText(DestinationRecords.RECORDS[2].destID)).check(matches(isDisplayed()))
+        onView(withText(DestinationRecords.RECORDS[1].destID)).check(matches(isDisplayed()))
 
         onView(withId(R.id.location_switch)).perform(click())
-        val uiDevice = UiDevice.getInstance(getInstrumentation())
-        uiDevice.pressHome()
+        Thread.sleep(1000)
+        onView(withId(R.id.drawer_layout))
+            .perform(DrawerActions.open())
+        onView(withId(R.id.routeFragment))
+            .perform(click())
+        onView(withId(R.id.fragment_route_directors_parent))
+            .check(matches(isDisplayed()))
+        Thread.sleep(4000)
+        onView(withId(R.id.drawer_layout))
+            .perform(DrawerActions.open())
+        onView(withId(R.id.roadBookFragment))
+            .perform(click())
+        onView(withId(R.id.refresh_button)).perform(click())
 
-        Thread.sleep(4000)
-        val device = UiDevice.getInstance(getInstrumentation())
-        device.pressHome()
-        device.pressRecentApps()
-        device.click(device.displayWidth / 2, device.displayHeight / 2)
-        device.click(device.displayWidth / 2, device.displayHeight / 2)
-        Thread.sleep(4000)
         // Now
-        swipeLeftTheRecordAt(2)
+        swipeLeftTheRecordAt(1)
         onView(withText(R.string.delete_dialog_title)).check(doesNotExist())
-        onView(withText(DestinationRecords.RECORDS[2].destID)).check(doesNotExist())
+        onView(withText(DestinationRecords.RECORDS[1].destID)).check(doesNotExist())
     }
 
     fun withIndex(matcher: Matcher<View?>, index: Int): Matcher<View?>? {

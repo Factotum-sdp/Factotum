9c675e5bfa4dcf1645c67ed4eb0cbd4b6b37bc4c
Carl Schütz <60784977+schuetzcarl@users.noreply.github.com>
2023-05-20 13:53:52 +0200

    Feature/courier bag (#231)
    * Add Bag menu button and moves EndShift to optionMenu
    
    * Refactor DRecordList API
    
    * Add timestampedSet field in DRecordList
    
    * Implement Bag Fragment and pack managment
    
    * adjust existing tests
    
    * Add some tests
    
    * Even more tests
    
    * Change for CI
    
    * Search ..
    
    * Move some tests
    
    * Refactor & Add doc
    
    * Add two more tests on timestamp post edit in RB
    
    * Refactor tests

---
+++ app/src/main/java/com/github/factotum_sdp/factotum/ui/bag/BagFragment.kt
@@ -0,0 +1,37 @@
+package com.github.factotum_sdp.factotum.ui.bag
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.recyclerview.widget.RecyclerView
+import com.github.factotum_sdp.factotum.R
+
+/**
+ * The "bag" Fragment
+ * Showing on screen all the packets delivered or currently delivered during a Shift
+ */
+class BagFragment: Fragment() {
+
+    private val bagViewModel: BagViewModel by activityViewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val view = inflater.inflate(R.layout.fragment_bag, container, false)
+        val adapter = BagAdapter()
+
+        bagViewModel.packages.observe(viewLifecycleOwner) {
+            adapter.submitList(it.toList())
+        }
+
+        val packagesRecyclerView: RecyclerView = view.findViewById(R.id.packagesRecyclerView)
+        packagesRecyclerView.adapter = adapter
+
+        return view
+    }
+}
